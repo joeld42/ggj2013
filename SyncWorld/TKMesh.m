e198bf3c1f36b7273e1e45400eb15d255838e28c
Joel Davis <joeld@tapnik.com>
2013-01-26 11:36:42 -0800

    add stuff

---
+++ SyncWorld/TKMesh.m
@@ -0,0 +1,18 @@
+//
+// Created by joeld on 1/25/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "TKMesh.h"
+
+
+@implementation TKMesh
+
++ (TKMesh *)meshFromObjFile:(NSString *)filename
+{
+
+}
+
+@end
